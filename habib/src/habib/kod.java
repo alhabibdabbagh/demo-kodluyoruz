5ae2691eaf2d934acced4f3254d52f4cdfc16730
alhabibdabbagh <58683636+alhabibdabbagh@users.noreply.github.com>
2020-03-07 12:36:45 +0300

    odev

---
+++ habib/src/habib/kod.java
@@ -0,0 +1,10 @@
+package habib;
+
+import java.util.Scanner;
+
+public class kod {
+	Scanner s=new Scanner(System.in);
+	int x=s.nextInt();
+	System.out.println("0-Diger Islemleri/n 1-Vize Islemleri/n 2- kredi karti islemleri/n 3-ev kredisi islemleri /n 4-musteri temsilcisi ");
+
+}
